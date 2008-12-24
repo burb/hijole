9a4a7df817c82514479733d00e4870c54df5deb6
Alexander Ershov <ershov.a_n@mail.ru>
2008-12-25 00:20:55 +0300

    Added metaclass for Relation

--- db.py
+++ db.py
@@ -1,26 +1,59 @@
+class Meta(type):
+    def __init__(self, name, superclasses, dict):
+        print("HA!")
+        type.__init__(self, name, superclasses, dict)
+        for name, element in dict.iteritems():
+            if isinstance(element, Element):
+                element.name = name
+                element.relation = self
+
+class Element(object):
+    def create(self):
+        raise NotImplementedError("My subclass must define create method")
+
+    def update(self, old):
+        raise NotImplementedError("My subclass must define update method")
+
+    def delete(self):
+        raise NotImplementedError("My subclass must define delete method")                
+
 class Relation(object):
+    __metaclass__ = Meta
     @classmethod
     def create(cls):
         columns = []
         for name, value in cls.__dict__.iteritems():
             if isinstance(value, Element):
-                columns.append(value.create(name))
+                columns.append(value.create())
         return "CREATE TABLE %s (%s);" % (cls.__name__, ', '.join(columns))
         
+class PrimaryKey(Element):
+    def __init__(self, *columns):
+        self.columns = columns
 
-class Element(object):
-    def create(self, name):
-        raise NotImplementedError("My subclass must define create method")
-
-    def update(self, old, name):
-        raise NotImplementedError("My subclass must define update method")
-
-    def delete(self, name):
-        raise NotImplementedError("My subclass must define delete method")
+    def create(self, *args):
+        return "PRIMARY KEY (%s)" % ', '.join(self.columns)
 
 class Column(Element):
-    def __init__(self, definition):
-        self.definition = definition
+    def __init__(self, type, default = None, can_be_null = False):
+        self.type = type
+        self.default = default
+        self.can_be_null = can_be_null
 
-    def create(self, name):
-        return "%s %s" % (name, self.definition)
+    def create(self):
+        return "%s %s %s %s" % (self.name, self.type, "DEFAULT %s" % self.default if self.default != None else "", "NOT NULL" if self.can_be_null == False else "")
+
+class TextColumn(Column):
+    def __init__(self, **kwargs):
+        Column.__init__(self, type = "TEXT", **kwargs)
+        
+class VarCharColumn(Column):
+    def __init__(self, length = 255, **kwargs):
+        Column.__init__(self, "VARCHAR(%d)" % length, **kwargs)
+
+class ReferenceColumn(Column):
+    def __init__(self, reference):
+        self.reference = reference
+
+    def create(self):
+        return "%s REFERENCES %s(%s)" % (self.name, self.reference.relation.__name__, self.reference.name)
--- post.py
+++ post.py
@@ -1,7 +1,14 @@
 import db
 
 class Post(db.Relation):
-    title = db.Column("VARCHAR(100)")
-    text = db.Column("TEXT")
+    post_id = db.Column(type = "INTEGER")
+    title = db.VarCharColumn(length = 100)
+    text = db.TextColumn()
+    pk = db.PrimaryKey("post_id")
+
+class Comment(db.Relation):
+    post_id = db.ReferenceColumn(Post.post_id)
+    text = db.TextColumn()
 
 print(Post.create())
+print(Comment.create())
