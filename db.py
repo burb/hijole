9c23be9154175cab00d5a5c9c43ef95d3cbf2ada
Alexander Ershov <ershov.a_n@mail.ru>
2008-12-25 23:28:49 +0300

    Added primary key support\

--- db.py
+++ db.py
@@ -2,10 +2,13 @@ class Meta(type):
     def __init__(self, name, superclasses, dict):
         print("HA!")
         type.__init__(self, name, superclasses, dict)
+        self.primary_key = []
         for name, element in dict.iteritems():
             if isinstance(element, Element):
                 element.name = name
                 element.relation = self
+                if element.primary_key:
+                    self.primary_key.append(element.name)
 
 class Element(object):
     def create(self):
@@ -25,7 +28,10 @@ class Relation(object):
         for name, value in cls.__dict__.iteritems():
             if isinstance(value, Element):
                 columns.append(value.create())
-        return "CREATE TABLE %s (%s);" % (cls.__name__, ', '.join(columns))
+        return "CREATE TABLE %s (%s %s);" % (cls.__name__,
+                                             ', '.join(columns),
+                                             ", PRIMARY KEY (%s)" % ', '.join(cls.primary_key) if cls.primary_key != [] else "")
+
         
 class PrimaryKey(Element):
     def __init__(self, *columns):
@@ -35,10 +41,11 @@ class PrimaryKey(Element):
         return "PRIMARY KEY (%s)" % ', '.join(self.columns)
 
 class Column(Element):
-    def __init__(self, type, default = None, can_be_null = False):
+    def __init__(self, type, default = None, can_be_null = False, primary_key = False):
         self.type = type
         self.default = default
         self.can_be_null = can_be_null
+        self.primary_key = primary_key
 
     def create(self):
         return "%s %s %s %s" % (self.name, self.type, "DEFAULT %s" % self.default if self.default != None else "", "NOT NULL" if self.can_be_null == False else "")
@@ -52,8 +59,11 @@ class VarCharColumn(Column):
         Column.__init__(self, "VARCHAR(%d)" % length, **kwargs)
 
 class ReferenceColumn(Column):
-    def __init__(self, reference):
+    def __init__(self, reference, **kwargs):
+        Column.__init__(self, None, **kwargs)
         self.reference = reference
 
     def create(self):
         return "%s REFERENCES %s(%s)" % (self.name, self.reference.relation.__name__, self.reference.name)
+
+    
--- post.py
+++ post.py
@@ -1,14 +1,13 @@
 import db
 
 class Post(db.Relation):
-    post_id = db.Column(type = "INTEGER")
+    post_id = db.Column(type = "INTEGER", primary_key = True)
     title = db.VarCharColumn(length = 100)
     text = db.TextColumn()
-    pk = db.PrimaryKey("post_id")
 
 class Comment(db.Relation):
-    post_id = db.ReferenceColumn(Post.post_id)
-    text = db.TextColumn()
+    post_id = db.ReferenceColumn(Post.post_id, primary_key = True)
+    text = db.TextColumn(primary_key = True)
 
 print(Post.create())
 print(Comment.create())
